2fcb8b3c2ffaefc77b3b670215639907533bf83c
elina <elina.paxar@gmail.com>
2023-03-14 22:29:51 +0300

    read config file

--- src/main.cpp
+++ src/main.cpp
@@ -1,10 +1,11 @@
-#include <iostream>
 #include "load_balancer_server.h"
+#include "conf_information.h"
 
 int main() {
     load_balancer_server server;
-
     server.start_server();
+    conf_information conf;
+    conf.get_conf();
 
     return 0;
 }
