48d0a67935ad297b561c9cf1a285460dbe599eeb
elina <elina.paxar@gmail.com>
2023-03-15 01:00:31 +0300

    function improvement

--- src/conf_information.cpp
+++ src/conf_information.cpp
@@ -3,25 +3,21 @@
 #include <iostream>
 
 
-void get_parameter(std::fstream &config_file, conf_information *this_ptr) {
+ void get_parameter(std::fstream &config_file, conf_information *this_ptr) {
     std::string config_string{};
 
     for (config_file >> config_string; !config_file.eof(); config_file >> config_string) {
-        if (config_string == "read_node") {
-            config_file >> config_string;
+        if (config_string == "read_node:") {
             config_file >> this_ptr->read_node;
-        } else if (config_string == "max_number_of_datagrams") {
-            config_file >> config_string;
+        } else if (config_string == "max_number_of_datagrams:") {
             config_file >> this_ptr->max_number_of_datagrams;
-        } else {
-            config_file >> config_string;
-            config_file >> config_string;
-
-            this_ptr->available_node.push_back(config_string);
+        } else if (config_string == "workers:"){
+            while ((config_file >> config_string) && !config_file.eof()) {
+                this_ptr->available_node.push_back(config_string);
+            }
         }
     }
 }
-
 
 void conf_information::get_conf() {
     std::fstream config_file("../src/load_balancer.conf");
@@ -30,10 +26,6 @@ void conf_information::get_conf() {
         get_parameter(config_file, this);
     } else {
         std::cout << "Unable to open config file\n";
-    }
-
-    for (size_t i = {0}; i < available_node.size(); i++) {
-        std::cout << available_node[i] << '\n';
     }
 
     config_file.close();
