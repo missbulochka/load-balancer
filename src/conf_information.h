d6b1060420558db68dc2c13b4ab233c074aaa21b
elina <elina.paxar@gmail.com>
2023-03-15 01:59:53 +0300

    update class & add some functions

--- src/conf_information.h
+++ src/conf_information.h
@@ -1,13 +1,22 @@
 #ifndef LOAD_BALANCER_CONF_INFORMATION_H
 #define LOAD_BALANCER_CONF_INFORMATION_H
 
-#include "iostream"
-#include "vector"
+#include <string>
+#include <vector>
 
 class conf_information {
 public:
-    int read_node{0};
-    int max_number_of_datagrams{0};
+    conf_information();
+
+    std::uint32_t get_port() const;
+
+    std::uint32_t get_max_number_of_datagrams() const;
+
+    std::vector<std::string> get_available_node();
+
+private:
+    std::uint32_t port{0};
+    std::uint32_t max_number_of_datagrams{0};
     std::vector<std::string> available_node{};
 
     void get_conf();
--- src/main.cpp
+++ src/main.cpp
@@ -1,14 +1,10 @@
-<<<<<<< HEAD
 #include "load_balancer_server.h"
-=======
->>>>>>> 9795b59 (format)
 #include "conf_information.h"
 
 int main() {
     load_balancer_server server;
     server.start_server();
     conf_information conf;
-    conf.get_conf();
 
     return 0;
 }
