a8eea4cf5bff4e89c0953d4dab5946f5665c4701
elina <elina.paxar@gmail.com>
2023-03-15 01:02:22 +0300

    format & delete useless line

--- src/conf_information.cpp
+++ src/conf_information.cpp
@@ -3,7 +3,7 @@
 #include <iostream>
 
 
- void get_parameter(std::fstream &config_file, conf_information *this_ptr) {
+void get_parameter(std::fstream &config_file, conf_information *this_ptr) {
     std::string config_string{};
 
     for (config_file >> config_string; !config_file.eof(); config_file >> config_string) {
@@ -11,7 +11,7 @@
             config_file >> this_ptr->read_node;
         } else if (config_string == "max_number_of_datagrams:") {
             config_file >> this_ptr->max_number_of_datagrams;
-        } else if (config_string == "workers:"){
+        } else if (config_string == "workers:") {
             while ((config_file >> config_string) && !config_file.eof()) {
                 this_ptr->available_node.push_back(config_string);
             }
@@ -27,6 +27,4 @@ void conf_information::get_conf() {
     } else {
         std::cout << "Unable to open config file\n";
     }
-
-    config_file.close();
 }
