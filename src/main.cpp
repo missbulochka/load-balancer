daafa832d1fab5de71d39f29ce059bf891549c67
elina <elina.paxar@gmail.com>
2023-03-16 00:20:53 +0300

    create server

--- src/main.cpp
+++ src/main.cpp
@@ -1,10 +1,13 @@
 #include "load_balancer_server.h"
 #include "config.h"
+#include "load_balancer_server.h"
 
 int main() {
     load_balancer_server server;
     server.start_server();
     config conf;
+    load_balancer_server server;
+    server.recv_datagram();
 
     return 0;
 }
