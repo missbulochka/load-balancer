96f66ef96d6e14f4876ee840802dfc20b504fec5
elina <elina.paxar@gmail.com>
2023-03-16 01:23:28 +0300

    delete useless variables, functions, etc

--- src/load_balancer_server.cpp
+++ src/load_balancer_server.cpp
@@ -1,13 +1,11 @@
 #include "load_balancer_server.h"
-#include <iostream>
 #include <sys/socket.h>
-#include <memory>
 
 load_balancer_server::load_balancer_server()
     : sockfd(-1)
     , port(5000)
-    , server_response(reinterpret_cast<const std::string*>("Message delivered successfully"))
-    , client_response()
+    , datagram()
+    , client_address_len(sizeof(client_addr))
     , client_addr{}
     , server_addr{AF_INET, htons(port), INADDR_ANY} {
     if ((sockfd = socket(AF_INET, SOCK_DGRAM, 0)) < 0) {
@@ -22,26 +20,16 @@ load_balancer_server::load_balancer_server()
 }
 
 void load_balancer_server::recv_datagram() {
-    std::string datagram{};
-    ssize_t datagram_size{0};
-    socklen_t client_address_len = sizeof(client_addr);
-
     while (true) {
-        datagram_size = recvfrom(sockfd,
-                                 reinterpret_cast<void*>(client_response),
-                                 sizeof(client_response),
-                                 MSG_WAITALL,
-                                 reinterpret_cast<struct sockaddr*>(&client_addr),
-                                 &client_address_len);
-
-        client_response[datagram_size] = '\0';
-        std::cout << client_response << '\n';
-        sendto(sockfd,
-               reinterpret_cast<const void*>(server_response),
-               server_response->size(),
-               MSG_CONFIRM,
-               reinterpret_cast<const struct sockaddr*>(&client_addr),
-               client_address_len);
-        std::cout << "Hello message sent." << std::endl;
+        if (recvfrom(sockfd,
+                     reinterpret_cast<void*>(datagram),
+                     sizeof(datagram),
+                     MSG_WAITALL,
+                     reinterpret_cast<struct sockaddr*>(&client_addr),
+                     &client_address_len)
+            == -1) {
+            perror("Error receiving datagram from socket");
+            exit(EXIT_FAILURE);
+        }
     }
 }
