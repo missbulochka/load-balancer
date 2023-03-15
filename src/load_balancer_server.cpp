7f48d4ccb99ce6ab190c254575f68b86374dd63c
elina <elina.paxar@gmail.com>
2023-03-16 01:00:38 +0300

    update class

--- src/load_balancer_server.cpp
+++ src/load_balancer_server.cpp
@@ -1,54 +1,47 @@
 #include "load_balancer_server.h"
 #include <iostream>
 #include <sys/socket.h>
-#include <netinet/in.h>
 #include <memory>
 
-
-load_balancer_server::load_balancer_server() = default;
-
-void load_balancer_server::recv_datagram() {
-    int sockfd{0};
-    std::uint16_t port{5000};
-    const auto* server_response = reinterpret_cast<const std::string*>("Message delivered successfully");
-    std::string client_response[1024];
-
+load_balancer_server::load_balancer_server()
+    : sockfd(-1)
+    , port(5000)
+    , server_response(reinterpret_cast<const std::string*>("Message delivered successfully"))
+    , client_response()
+    , client_addr{}
+    , server_addr{AF_INET, htons(port), INADDR_ANY} {
     if ((sockfd = socket(AF_INET, SOCK_DGRAM, 0)) < 0) {
         perror("Socket creation failed");
         exit(EXIT_FAILURE);
     }
 
-    struct sockaddr_in client_addr {};
-
-    struct sockaddr_in server_addr {
-        AF_INET, htons(port), INADDR_ANY,
-    };
-
     if (bind(sockfd, reinterpret_cast<const struct sockaddr*>(&server_addr), sizeof(server_addr)) < 0) {
         perror("Bind failed");
         exit(EXIT_FAILURE);
     }
+}
 
+void load_balancer_server::recv_datagram() {
     std::string datagram{};
     ssize_t datagram_size{0};
     socklen_t client_address_len = sizeof(client_addr);
 
     while (true) {
         datagram_size = recvfrom(sockfd,
-                                 reinterpret_cast<std::string*>(client_response),
-                                 client_response->size(),
+                                 reinterpret_cast<void*>(client_response),
+                                 sizeof(client_response),
                                  MSG_WAITALL,
                                  reinterpret_cast<struct sockaddr*>(&client_addr),
                                  &client_address_len);
 
+        client_response[datagram_size] = '\0';
         std::cout << client_response << '\n';
         sendto(sockfd,
-               reinterpret_cast<const char*>(server_response),
+               reinterpret_cast<const void*>(server_response),
                server_response->size(),
                MSG_CONFIRM,
                reinterpret_cast<const struct sockaddr*>(&client_addr),
                client_address_len);
-        std::cout<<"Hello message sent."<<std::endl;
+        std::cout << "Hello message sent." << std::endl;
     }
-
 }
--- src/load_balancer_server.h
+++ src/load_balancer_server.h
@@ -1,10 +1,26 @@
 #ifndef LOAD_BALANCER_LOAD_BALANCER_SERVER_H
 #define LOAD_BALANCER_LOAD_BALANCER_SERVER_H
 
+#include <cstdint>
+#include <string>
+#include <netinet/in.h>
+
 class load_balancer_server {
 public:
     load_balancer_server();
     void recv_datagram();
+
+private:
+    int sockfd;
+    std::uint16_t port;
+    const std::string* server_response;
+    std::string client_response[1024];
+
+    struct sockaddr_in client_addr;
+
+    struct sockaddr_in server_addr {
+        AF_INET, htons(port), INADDR_ANY,
+    };
 };
 
 
