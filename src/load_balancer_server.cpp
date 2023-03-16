6f83ab0246e412019c258cbb9cf9ced33513886a
elina <elina.paxar@gmail.com>
2023-03-16 13:29:06 +0300

    add handler

--- src/load_balancer_server.cpp
+++ src/load_balancer_server.cpp
@@ -1,6 +1,7 @@
 #include "load_balancer_server.h"
 #include <sys/socket.h>
 #include <iostream>
+#include <csignal>
 
 load_balancer_server::load_balancer_server()
     : sockfd()
@@ -12,24 +13,29 @@ load_balancer_server::load_balancer_server()
     start_server();
 }
 
+
+void load_balancer_server::signal_handler(int signal_num) {
+    std::cout << "Interrupt signal (" << signal_num << "reveived\n";
+    exit(signal_num);
+}
+
 void load_balancer_server::start_server() {
     if ((sockfd = socket(AF_INET, SOCK_DGRAM, 0)) < 0) {
         perror("Socket creation failed");
         exit(EXIT_FAILURE);
     }
 
-    if (bind(sockfd,
-             reinterpret_cast<const struct sockaddr*>(&server_addr),
-             sizeof(server_addr))
-        < 0) {
+    if (bind(sockfd, reinterpret_cast<const struct sockaddr*>(&server_addr), sizeof(server_addr)) < 0) {
         perror("Bind failed");
         exit(EXIT_FAILURE);
     }
+
+    signal(SIGTERM, signal_handler);
 }
 
 void load_balancer_server::recv_datagram() {
     while (true) {
-        if (recvfrom(sockfd,
+        if (recvfrom(0,
                      reinterpret_cast<void*>(datagram),
                      sizeof(datagram),
                      MSG_WAITALL,
@@ -37,7 +43,7 @@ void load_balancer_server::recv_datagram() {
                      &client_addr_len)
             == -1) {
             perror("Error receiving datagram from socket");
-            exit(EXIT_FAILURE);
+            continue ;
         }
         std::cout << "Datagram received\n";
     }
