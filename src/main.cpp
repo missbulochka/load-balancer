445abdc404a224e1dbabda00c8e326d63044cc4e
elina <elina.paxar@gmail.com>
2023-03-17 10:13:46 +0300

    fixed mistake (in main) & add func

--- src/main.cpp
+++ src/main.cpp
@@ -1,14 +1,10 @@
 #include "load_balancer_server.h"
 #include "config.h"
-#include "load_balancer_server.h"
 
 int main() {
     load_balancer_server server;
     server.start_server();
     config conf;
-    load_balancer_server server;
-
-    server.start_server();
 
     return 0;
 }
