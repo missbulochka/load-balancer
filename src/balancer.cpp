61ccbf741b4900723f842094a3bf3c75af5236f0
elina <elina.paxar@gmail.com>
2023-03-19 21:46:57 +0300

    work stop update

--- src/balancer.cpp
+++ src/balancer.cpp
@@ -1,10 +1,10 @@
 #include "balancer.h"
-#include <thread>
 #include <iostream>
 
 balancer::balancer()
-    : flag(true)
-    , datagram() {
+    : exit_flag(false)
+    , datagram()
+    , balancer_thread(&balancer::balancer_run, this) {
     start_balancer();
 }
 
@@ -12,25 +12,31 @@ void balancer::start_balancer() {
     server.start_server(conf.get_port());
     client.start_client();
 
-    std::thread balancer_thread(&balancer::balancer_work, this);
-
     getchar();
-    flag = false;
-
-    balancer_thread.join();
+    stop_balancer();
 }
 
-void balancer::balancer_work() {
-    while (flag) {
+void balancer::balancer_run() {
+    std::cout << "Balancer start to work\n";
+    while (!exit_flag) {
         if (!(datagram = server.recv_datagram()).empty()) {
-            client.send_datagram(std::make_pair("127.55.6.1", 5001), &datagram);
-        } else {
+            client.send_datagram(get_next_node(), &datagram);
+        }
+        else {
             std::cout << "Datagram is not received\n";
         }
     }
 }
 
 std::pair<std::string, std::uint16_t> balancer::get_next_node() {
-    auto a = std::make_pair("127.0.0.1", 5001);
+    auto a = std::make_pair("127.55.6.1", 5001);
     return a;
+}
+
+void balancer::stop_balancer() {
+    std::cout << "Balancer stop to work\n";
+    exit_flag = true;
+    balancer_thread.join();
+    server.stop_server();
+    client.stop_client();
 }
--- src/balancer.h
+++ src/balancer.h
@@ -4,21 +4,24 @@
 #include "config.h"
 #include "load_balancer_server.h"
 #include "load_balancer_client.h"
+#include <thread>
 
 class balancer {
 public:
     balancer();
 
 private:
-    bool flag;
+    bool exit_flag;
     std::string datagram;
     config conf;
     load_balancer_server server;
     load_balancer_client client;
+    std::thread balancer_thread;
 
     void start_balancer();
-    void balancer_work();
+    void balancer_run();
     std::pair<std::string, std::uint16_t> get_next_node();
+    void stop_balancer();
 };
 
 
